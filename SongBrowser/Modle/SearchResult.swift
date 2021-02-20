30b3adf29bdfcbed0abee03d4b0399c6ad538fa1
lcs-dsmith <dsmith@lcs.on.ca>
2021-02-20 10:15:45 -0500

    Defined the model for the parts of the JSON file we care about retrieving

---
+++ SongBrowser/Modle/SearchResult.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResult.swift
+//  SongBrowser
+//
+//  Created by Dylan Smith on 2021-02-20.
+//
+
+import Foundation
+
+struct searchResult: Decodable {
+    
+    let results: [Song]
+    
+}
--- SongBrowser/Veiws/ContentView.swift
+++ SongBrowser/Veiws/ContentView.swift
@@ -42,6 +42,13 @@ struct ContentView: View {
     }
 }
 
+
+
+
+
+
+
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
