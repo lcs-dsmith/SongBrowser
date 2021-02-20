77e0992461140899235ec3ca44277e465718dd6b
lcs-dsmith <dsmith@lcs.on.ca>
2021-02-20 09:49:45 -0500

    Initial Commit

---
+++ SongBrowser/SongBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  SongBrowserApp.swift
+//  SongBrowser
+//
+//  Created by Dylan Smith on 2021-02-20.
+//
+
+import SwiftUI
+
+@main
+struct SongBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
